c4bd3c084e9f80265ab644f98b62f81f69eb7971
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2025-08-10 16:19:10 +0530

    Update GameEditor.cpp

--- Editor/GameEditor.cpp
+++ Editor/GameEditor.cpp
@@ -309,8 +309,8 @@ void GameEditor::DrawSceneWindow()
 	(
 		(ImTextureID)(intptr_t)m_RaylibTexture.texture.id,
 		ImGui::GetContentRegionAvail(),
-		ImVec2(0, 1),  // Bottom-left UV
-		ImVec2(1, 0),   // Top-right UV (flipped vertically)
+		ImVec2(0, 1),		// Bottom-left UV
+		ImVec2(1, 0),		// Top-right UV (flipped vertically)
 		ImVec4(1, 1, 1, 1), // Tint color (no change)
 		ImVec4(0, 0, 0, 0)  // No border
 	);
