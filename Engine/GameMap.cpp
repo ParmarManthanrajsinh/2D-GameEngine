bffe840c8eb38c0fcc874aadf702b81d37182381
Dhaval <dp9718776@gmail.com>
2025-11-29 22:41:52 +0530

    Minor change

--- Engine/GameMap.cpp
+++ Engine/GameMap.cpp
@@ -62,6 +62,6 @@ void GameMap::RequestGotoMap(std::string_view map_id, bool force_reload)
     }
     else
     {
-		std::cerr << "Transition callback not set!" << std::endl;
+		std::cout << "Transition callback not set!" << std::endl;
     }
 }
